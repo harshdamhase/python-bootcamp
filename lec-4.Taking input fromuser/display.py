d3613372430e7dfecc56fa2680418d090affcd50
Harshda Mhase <harshadamhase12@gmail.com>
2023-06-09 19:06:34 -0900

    doc: formatted-output

---
+++ lec-4.Taking input fromuser/display.py
@@ -0,0 +1,4 @@
+name="harshda"
+age=21
+dob="26-11-2001"
+print('hello {}. my age is {}.My date of birth is {}'.format(name,age))
--- lec3.Datatypes&Variable/type.py
+++ lec3.Datatypes&Variable/type.py
@@ -4,3 +4,5 @@ weight = 40
 print(type(name))
 print(type(age))
 print(type(weight))
+
+#type function check type of function
