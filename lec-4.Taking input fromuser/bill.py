0e635b4f5489ce7f779f95868d046fcc95457bdd
Harshda Mhase <harshadamhase12@gmail.com>
2023-06-13 18:24:56 -0900

    doc: bill-program-added

---
+++ lec-4.Taking input fromuser/bill.py
@@ -0,0 +1,5 @@
+rate = input("Enter rate of bag")
+quantity = input("Enter quantity")
+
+bill = int(rate*quantity)
+print("Bill",bill)
---
+++ lec-4.Taking input fromuser/index.py
@@ -0,0 +1,4 @@
+a = input("Enter a")
+b = input("enter b")
+print("a",a)
+print("b",b)
