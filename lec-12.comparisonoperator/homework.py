e14944ccd7e7a885d195e820e9ab4dc2d5a89fff
Harshda Mhase <harshadamhase12@gmail.com>
2023-06-23 19:04:14 -0900

    ref: homework-eg-added

--- lec-12.comparisonoperator/homework.py
+++ lec-12.comparisonoperator/homework.py
@@ -1,6 +1,10 @@
+# user is elible for voting
+
 user = 19
 if(user>18):
   print('user is eligible for voting')
+
+# check num is even or odd
 
 num = 10
 if(num%2==0):
@@ -8,4 +12,3 @@ if(num%2==0):
 else:
   print("Given num is odd")  
 
-  
