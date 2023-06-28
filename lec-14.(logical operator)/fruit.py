878827655b9b6072b8fdb78e33f6f92921a155c1
Harshda Mhase <harshadamhase12@gmail.com>
2023-06-27 20:34:12 -0900

    ref: fruit-eg-added

---
+++ lec-14.(logical operator)/fruit.py
@@ -0,0 +1,15 @@
+fruit = (input("Enter fruit"))
+if fruit=="🍇":
+  print("grapes")
+elif fruit=="🍎":
+  print("apple")
+elif fruit=="🍍":
+  print("pineapple")
+else:
+  print("not available")
+   
+    
+
+
+
+
