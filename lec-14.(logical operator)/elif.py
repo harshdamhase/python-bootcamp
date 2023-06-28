d090ed75d0ceee9c31aa83c58118dd590ff791db
Harshda Mhase <harshadamhase12@gmail.com>
2023-06-27 18:36:57 -0900

    ref: logical-operator-code-added

---
+++ lec-14.(logical operator)/elif.py
@@ -0,0 +1,15 @@
+num = int(input("Enter number from 1-5 :"))
+if num ==1:
+  print("You select option1")
+elif num ==2:
+  print("You select option-2")
+elif num == 3:
+  print("You select option-3")
+elif num == 4:
+  print("You select option-4")
+else:
+  print("You select option wrong option")        
+   
+
+#    Enter number from 1-5 :7
+# You select option wrong option
