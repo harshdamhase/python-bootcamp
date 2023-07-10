b0b344d16a5e1095b2c47a3ecbfb2f37c9cddff3
Harshda Mhase <harshadamhase12@gmail.com>
2023-07-10 10:49:17 -0900

    ref: function-added-in-python

---
+++ lec-13.programs/lec-15.function/fun-call.py
@@ -0,0 +1,14 @@
+def greeting():
+  print("Good Morning")
+  print("Good Evening")
+  print("Good Night")
+greeting()
+greeting()  
+
+
+# Good Morning
+# Good Evening
+# Good Night
+# Good Morning
+# Good Evening
+# Good Night
